02573eff8f6e5bce9151fa6db7cb49ea395bf82f
Vladyslav Diachenko <basher207vd@gmail.com>
2023-10-30 01:46:26 +0200

    Changed code logic in controllers/filters/filter.js for getFilteredBooks function

--- controllers/author.js
+++ controllers/author.js
@@ -4,7 +4,6 @@ const { getUniqueValues } = require("./filters/filter");
 const authorController = () => {
     const allAuthors = data.map(book => book.author);
     return getUniqueValues(allAuthors);
-    
 };
 
 module.exports = authorController;
--- controllers/filters/filter.js
+++ controllers/filters/filter.js
@@ -1,39 +1,34 @@
-const getFilteredBooks = (data, querys) => {
-    const validquerys = Object.fromEntries(
-        Object.entries(querys).filter((kvArr) => {
-            if(kvArr[0] === 'filter'){
-                return (kvArr[1] === 'year' || kvArr[1] ==='price');
-            }
-            if(kvArr[0] === 'type'){
-                return (kvArr[1] === 'gte' || kvArr[1] === 'lte');
-            }
-            if(kvArr[0] === 'value'){
-                return !isNaN(+kvArr[1]);
-            }
-        })
-    );
-    
-    if(Object.keys(validquerys).length === 3){
-        if(validquerys.filter === 'year'){
-            validquerys.filter = 'publicationYear'
-        }
-        
-        const result = data.filter((book) => {
-            let bool;
-            validquerys.type === 'gte' ? 
-            bool = book[validquerys.filter] >= +validquerys.value : 
-            bool = book[validquerys.filter] <= +validquerys.value
-            return bool;
+const getFilteredBooks = (data, query) => {
+    const validQuery = {
+        filter: ['year', 'price'],
+        type: ['gte', 'lte']
+    };
+    const hasValidQueries = () => {
+        return Object.keys(validQuery).every((key) => {
+            return validQuery[key].includes(query[key]);
         });
-        return result
+    };
+    const operators = {
+        gte: a => a >= query.value,
+        lte: a => a <= query.value
+    };
+
+    if( isNaN(query.value) || !hasValidQueries() ){
+        throw new Error('Wrong queries');
+    };
+
+    const filterFn = (item) => {
+        return query.filter === 'year' ?  
+        operators[query.type](item.publicationYear) : 
+        operators[query.type](item.price)
     }
-    throw new Error('Wrong queries');
+    return data.filter(filterFn);
 };
 
 const getUniqueValues = (array) => {
     return array.filter((el, index) => {
         return array.indexOf(el) === index;
     });
-}
+};
 
 module.exports = { getFilteredBooks, getUniqueValues };
